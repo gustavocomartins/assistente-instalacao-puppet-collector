2aeffafa7fe62ac1a574d0728fe66874841a01ab
Gustavo C. de O. Martins <gustavo.omartins@bandtec.com.br>
2022-05-21 19:29:41 -0300

    tabelas db backup local

---
+++ tables.sql
@@ -0,0 +1,25 @@
+use collectorBackup;
+
+create table dadosColetados(
+id int primary key auto_increment,
+fkMaquinaVirtual int,
+usoDisco double,
+usoRAM double,
+usoProcessador double,
+datahora datetime
+)auto_increment = 1;
+
+create table controleDeAcesso(
+id int primary key auto_increment,
+fkUsuario int,
+fkmaquinaVirtual int,
+dataHora datetime
+)auto_increment = 1;
+
+create table alertas(
+id int primary key  auto_increment,
+fkDadosColetados int not null,
+dataHora datetime,
+descricao varchar(255),
+categoria varchar(10)
+)auto_increment = 1;
