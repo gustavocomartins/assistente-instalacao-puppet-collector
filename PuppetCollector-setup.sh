eb8bf9f8f5dddaf9482bd6cc3efb6c68a68acaf5
Gustavo C O Martins <gustavo.omartins@bandtec.com.br>
2022-05-24 01:02:15 -0700

    update setup y

--- PuppetCollector-setup.sh
+++ PuppetCollector-setup.sh
@@ -150,8 +150,8 @@ instalarCollector(){
 	echo "|| >>> Instalando Puppet Collector                        ||"
 	echo "============================================================"
 	cd /home/urubu100/setup
-	wget -O PuppetColector.jar https://github.com/gustavocomartins/puppet-colector-exe/raw/main/Puppet%20Colector.jar
-	chmod +x PuppetColector.jar
+	wget -O PuppetCollector.jar https://github.com/gustavocomartins/puppet-collector-exe/raw/main/Puppet%20Collector.jar
+	chmod +x PuppetCollector.jar
 	echo ">>> Puppet Collector instalado com sucesso."
 }
 
